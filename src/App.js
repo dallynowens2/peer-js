0c1e49ab76290fb039c7d5572a7e20417287abc5
dallynowens2 <dallynowens2@gmail.com>
2022-01-27 20:38:12 -0700

    added stuff

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
 import logo from './logo.svg';
+import PeerjsService from './services/PeerjsService';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [peerId, setPeerId] = useState()
+  const [message, setMessage] = useState('')
+
+  useEffect(()=>{
+    PeerjsService.initialize();
+  },[])
+
+  const messageChangeHandler =(e)=>{
+    setMessage(e.target.value)
+  }
+
+  const sendMessageHandler =()=>{
+    PeerjsService.sendMessage(message);
+    setMessage('')
+  }
+
+  const onPeerIdChange = (e) =>{
+    setPeerId(e.target.value)
+  }
+
+  const onPeerSubmit = (e) =>{
+    e.preventDefault()
+    PeerjsService.connectPeer(peerId);
+    setPeerId('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={onPeerSubmit}>
+            <label>Peer Id</label>
+            <input type='text' onChange={onPeerIdChange}></input>
+            <button type='submit'>Add Peer</button>
+        </form>
+        <div>
+          <label>Enter Message</label>
+          <input type='text' onChange={messageChangeHandler}></input>
+          <button type='button' onClick={sendMessageHandler}>Send Message</button>
+        </div>
+        <div>
+          
+        </div>
     </div>
   );
 }
---
+++ src/services/PeerjsService.js
@@ -0,0 +1,55 @@
+import Peer from 'peerjs';
+
+
+const id = randId();
+const peer = new Peer(id)
+const connections = []
+const messages =[]
+
+const initialize = () => {
+    console.log(id)
+    console.log(peer.id)
+    peer.on('open', (id) => {
+        console.log('id:' + id)
+    })
+    peer.on('connection', (conn) => {
+        connections.push(conn)
+        console.log('connected to:' + conn.peer)
+
+    })
+    peer.on('error', (err) => {
+        console.log(err);
+    })
+}
+
+const connectPeer = (peerId) =>{
+    const conn = peer.connect(peerId);
+    conn.on('open', ()=>{
+        connections.push(conn);
+        console.log(peerId)
+    })
+    conn.on('data', (data)=>{
+        console.log(data)
+        messages.push(data);
+    })
+}
+
+const sendMessage =(message)=>{
+    messages.push(message);
+    connections.forEach(c => c.send(message));
+}
+
+const randId = () => {
+    let roomLength = 6;
+    let lowChar = "(".charCodeAt(0);
+    let highChar = "Z".charCodeAt(0);
+    let possibleChars = highChar - lowChar + 1;
+    let randChar = () => {
+        let r = Math.round(Math.random() * possibleChars) + lowChar;
+        return String.fromCharCode(r);
+    };
+    return [...new Array(roomLength).keys()].map(randChar).join("");
+}
+
+const PeerjsService = {sendMessage, initialize, connectPeer}
+export default PeerjsService;
